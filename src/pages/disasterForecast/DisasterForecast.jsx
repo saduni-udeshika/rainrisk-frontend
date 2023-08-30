7f947cad7e0eee288c00425d4053056ee50222db
nsrajapaksha <nsrajapaksha0310@gmail.com>
2023-08-30 22:17:30 +0530

    Disaster Forecast Forntend

--- src/pages/disasterForecast/DisasterForecast.jsx
+++ src/pages/disasterForecast/DisasterForecast.jsx
@@ -1,53 +1,61 @@
 import React, { useState } from 'react';
-import axios from 'axios';
-import styles from './DisasterForecast.module.scss';
+import { Button, Input } from '../../components';
+import styles from './DisasterForecast.scss';
+import { disasterForecast } from '../../services/disaster.forecast.service';
 
-const DisasterForecast = () => {
+export const DisasterForecast = () => {
   const [date, setDate] = useState('');
   const [location, setLocation] = useState('');
   const [prediction, setPrediction] = useState('');
 
   const handlePredict = async () => {
-    try {
-      const response = await axios.post('/predict-disaster', {
-        date,
-        location,
-      });
-      setPrediction(response.data);
-    } catch (error) {
-      console.error('Error predicting disaster:', error);
+    if (location && date) {
+      try {
+        const response = await disasterForecast(location, date);
+        setPrediction(response.data);
+
+        // Clear the input fields after successful prediction
+        clearInputs();
+      } catch (error) {
+        console.error('Error predicting disaster:', error);
+      }
     }
   };
 
+  const clearInputs = () => {
+    setDate('');
+    setLocation('');
+    setPrediction('');
+  };
+
   return (
-    <div className={styles.container}>
-      <h2>Disaster Forecast Prediction</h2>
-      <div className={styles.inputGroup}>
-        <label>Date: </label>
-        <input type="text" value={date} onChange={(e) => setDate(e.target.value)} />
-      </div>
-      <div className={styles.inputGroup}>
-        <label>Location: </label>
-        <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} />
-      </div>
-      <button className={styles.predictButton} onClick={handlePredict}>
-        Predict Disaster
-      </button>
-      {prediction && (
-        <div className={styles.predictionResult}>
-          <h3>Prediction Result:</h3>
-          <p>Disaster Occurrence: {prediction['Disaster Occurrence']}</p>
-          {prediction['Disaster Occurrence'] === 'Yes' && (
-            <>
-              <p>Disaster Type: {prediction['Disaster Type']}</p>
-              <p>Severity: {prediction['Severity']}</p>
-              <p>Nearest Shelter Distance (km): {prediction['Nearest Shelter_km']}</p>
-            </>
-          )}
+    <div className={styles.disasterForecast}>
+      <div className={styles.leftSection}>
+        <div className={styles.assessForm}>
+          <div className={styles.assessedHeader}>Disaster Forecast Prediction</div>
+          <Input type="location" label="Location" value={location} onChange={(e) => setLocation(e.target.value)} />
+          <Input type="date" label="Date" value={date} onChange={(e) => setDate(e.target.value)} />
+          <Button label="Prediction" expand onClick={handlePredict} />
         </div>
-      )}
+        {prediction && (
+          <div className={styles.predictionResult}>
+            <h3>Prediction Result:</h3>
+            <p>Disaster Occurrence: {prediction['Disaster Occurrence']}</p>
+            {prediction['Disaster Occurrence'] === 'Yes' && (
+              <>
+                <p>Disaster Type: {prediction['Disaster Type']}</p>
+                <p>Severity: {prediction['Severity']}</p>
+                <p>Nearest Shelter Distance (km): {prediction['Nearest Shelter_km']}</p>
+              </>
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
 
 export default DisasterForecast;
+
+
+
